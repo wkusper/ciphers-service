0d9b8eb951e7535dca67f988c44569477de78cb9
Will Kusper <wkusper@iu.edu>
2024-02-29 19:40:08 -0500

    commit

--- ciphers_project/ciphers_project/settings.py
+++ ciphers_project/ciphers_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u0-am6k0!3@%3-41mwh=2olqmom)b)x#l5#5i^u71z$f9e2d6f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', 'https://ciphers-service-latest-6mmr.onrender.com']
 
 
 # Application definition
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://wkusper.github.io",
+    "http://localhost:3000"
+]
